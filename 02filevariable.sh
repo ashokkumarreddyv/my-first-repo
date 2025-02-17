40fd7e866812b0571bf8fb08d44e98e66ff30c9a
ashokv <ashok3akr@gmail.com>
2025-02-17 20:03:24 +0530

    learn

---
+++ 02filevariable.sh
@@ -0,0 +1,18 @@
+# syntax
+# function name() {
+#commands
+
+}
+
+# access the name
+# function name
+
+# example
+example() {
+  echo example function
+  echo value X - $X
+  }
+
+  X=10
+  example
+
